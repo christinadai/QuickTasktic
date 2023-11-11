113c8a2b12dc7664678df9d7147d5176a7bac818
christinadai <christinadaiok@gmail.com>
2023-11-11 12:34:33 -0800

    Update backend.py

--- backend.py
+++ backend.py
@@ -1,7 +1,9 @@
-#hey
-#we are going to win
-# babba
-#hi
-#j\ysud
-#whats
-#hi
+num_of_tasks = 0  
+tasks = []       # An empty list to store task objects
+curr_time = 0.0  
+departure_time = 0.0  
+curr_coordinate_x = 0.0  
+curr_coordinate_y = 0.0  
+starting_coordinate_x = 37.3491386955373  
+starting_coordinate_y = -121.9367374882497  
+num_of_time_sen_event = 0  
