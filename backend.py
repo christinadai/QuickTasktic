7372041ddcc25d1483bc72caa1bf6f36664aeb8a
gwynethanawalt <gwynethanawalt@gmail.com>
2023-11-11 12:56:36 -0800

    Update backend.py

--- backend.py
+++ backend.py
@@ -15,7 +15,7 @@ starting_coordinate_x = 37.3491386955373
 starting_coordinate_y = -121.9367374882497  
 num_of_time_sen_event = 0
 
-
+#time conversion 
 Def timetodec(timestr):
 	Try:
 		hrs, min = map( int, timestr.split(‘:’)
