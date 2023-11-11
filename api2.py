9de8e5f63754c0e5741e5457eb5302a4c43a7849
i-kennar <ian.kennar@icloud.com>
2023-11-11 15:59:41 -0800

    Added get_uas token function for parking api

--- api2.py
+++ api2.py
@@ -1,1 +1,16 @@
+import requests
+import json
+
+def get_uas():
+    url = "https://api.iq.inrix.com/auth/v1/appToken?appId=exhblj9x47&hashToken=ZXhoYmxqOXg0N3xwdjBNa3NEdkg1NmxOSXl6Z3dVd2QzWXlEelpheE5wOTZIMlYwS2dY"
+
+    payload = {}
+    headers = {}
+
+    response = requests.request("GET", url, headers=headers, data=payload)
+
+    print(response.text)
+
+    return (response.json()["result"]["token"])
+
 
