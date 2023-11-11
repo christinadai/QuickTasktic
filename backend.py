64b932dda482d24d42a5e3990591ba15c54ab9b0
christinadai <christinadaiok@gmail.com>
2023-11-11 14:58:50 -0800

    Update backend.py

--- backend.py
+++ backend.py
@@ -17,26 +17,7 @@ def find_min_ts_task(task_array):
         return min_ts_task
     else:
         return None
-
-#variables
-num_of_tasks = 0 
-tasks = []       # An empty list to store task objects
-departure_time_string = ''
-departure_time = timetodec(departure_time_string) 
-curr_time = departure_time 
-curr_coordinate_x = 0.0 
-curr_coordinate_y = 0.0 
-starting_coordinate_x = 37.3491386955373 
-starting_coordinate_y = -121.9367374882497 
-num_of_time_sen_event = 0
-
-class Task:
-    def __init__(task_name, time_sensitive=False, ts_time=-1.0, specific_location=False):
-        task_name.time_sensitive = time_sensitive
-        task_name.ts_time = ts_time
-        task_name.specific_location = specific_location
-
-
+    
 #sortandrank
 def calculate_rank(park_rank, travel_time):
     rank = (0.3 * park_rank) + (0.7 * travel_time)
@@ -51,4 +32,29 @@ def sort_rank(rank):
                 rank[j] = rank[j + 1]
                 rank[j + 1] = temp
 
-    return rank		
+    return rank	
+
+#variables
+num_of_tasks = 0 
+tasks = []       # An empty list to store task objects
+departure_time_string = ''
+departure_time = timetodec(departure_time_string) 
+curr_time = departure_time 
+curr_coordinate_x = 0.0 
+curr_coordinate_y = 0.0 
+starting_coordinate_x = 37.3491386955373 
+starting_coordinate_y = -121.9367374882497 
+num_of_time_sen_event = 0
+
+class Task:
+    def __init__(task_name, time_sensitive=False, ts_time_string='100:00', specific_location=False, specific_coordX = 0.0, specific_coordY = 0.0):
+        task_name.time_sensitive = time_sensitive
+        task_name.ts_time_string = ts_time_string
+        task_name.ts_time = ts_time
+        task_name.specific_location = specific_location
+        task_name.specific_coordX = specific_coordX
+        task_name.specific_coordY = specific_coordY
+
+ts_time = 0.0
+ts_time = timetodec(ts_time)
+
