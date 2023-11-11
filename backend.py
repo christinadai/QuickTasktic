d846c00809d1118bbfdeec4976125dfb5a16f407
rkiniris <150318680+rkiniris@users.noreply.github.com>
2023-11-11 12:49:14 -0800

    Update backend.py

--- backend.py
+++ backend.py
@@ -1,7 +1,8 @@
-#hey
-#we are going to win
-# babba
-#hi
-#j\ysud
-#whats
-#hi
+Def timetodec(timestr):
+
+	Try:
+		hrs, mins = map(int, timestr.split(‘:’))
+		dectime =(hrs+mins)/60.0
+		Return dectime
+	Except value error:
+		print(“Invalid time!”)
