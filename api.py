8ad3fec2e1cec955827bba3d5ff63b1c572a222e
anikas2003 <anikas2003@gmail.com>
2023-11-11 18:11:33 -0800

    all inrix apis are functional

--- api.py
+++ api.py
@@ -1,6 +1,7 @@
 import requests
 import json
 import xml.etree.ElementTree as ET
+import xmltodict
 
 def get_uas():
 
@@ -12,6 +13,7 @@ def get_uas():
     response = requests.request("GET", url, headers=headers, data=payload)
 
     return (response.json()["result"]["token"])
+
 
 def find_route(lat_1, long_1, lat_2, long_2):
     auth = get_uas()
@@ -25,14 +27,17 @@ def find_route(lat_1, long_1, lat_2, long_2):
 
     response = requests.request("GET", url, headers=headers, data=payload)
 
-    return (response.json()["routes"]["id"])
+    id = response.json().get('result', {}).get('trip', {}).get('routes', [{}])[0].get('id')
+    
+    return (id)
 
 
-def route_travel_times(count, time_interval, lat_1, long_1, lat_2, long_2):
+#Defauly departure time given in case not provided by user
+def route_travel_times(count, time_interval, lat_1, long_1, lat_2, long_2, dep_time = "2023-11-12T01:23:00Z"):
     auth = get_uas()
     route_id = find_route(lat_1, long_1, lat_2, long_2)
 
-    url = f"https://api.iq.inrix.com/routeTravelTimes?routeId={route_id}&travelTimeCount={count}&travelTimeInterval={time_interval}"
+    url = f"https://api.iq.inrix.com/routeTravelTimes?routeId={route_id}&departureTime={dep_time}&travelTimeCount={count}&travelTimeInterval={time_interval}"
 
     payload = {}
     headers = {
@@ -40,16 +45,44 @@ def route_travel_times(count, time_interval, lat_1, long_1, lat_2, long_2):
     }
 
     response = requests.request("GET", url, headers=headers, data=payload)
-    # Parse the XML response
-    root = ET.fromstring(response)
-    #Access the specific elements that we need
     
+    #Convert XML/Text file (response) to JSON (json_data)
+
+    xml_dict = xmltodict.parse(response.text)
+
+    # Convert the Python dictionary to string
+    str_data = json.dumps(xml_dict, indent=2)  # indent for pretty printing
+
+    #Convert data from string to json
+    parsed_json = json.loads(str_data)
+
+    # Use get to get info
+    travel_time = parsed_json.get('Inrix', {}).get("Trip", {}).get("Route", {}).get("TravelTimes", {}).get("TravelTime", [{}])[0].get("@travelTimeMinutes")
     
-    return 
+    return travel_time
+
+#Half a mile radius, maximum 10 minute walk
+def street_parking(lat, long, rad = 800):
+    auth = get_uas()
+
+    url = f"https://api.iq.inrix.com/blocks/v3?point={lat}%7C-{long}&radius={rad}"
+
+    payload = {}
+    headers = {
+      'Authorization': f'Bearer {auth}'
+    }
+
+    response = requests.request("GET", url, headers=headers, data=payload)
     
-
-    #return response.json()
-
+    return (response.json())
 
 
-route_travel_times(15, 45, 37.352100, -121.936780, 37.356930, -121.936630)
+
+
+"""
+#testing:
+#street_parking(37.74304518280319, 122.42438793182373, 50)
+#route_travel_times(15, 45, 37.770581, -122.442550, 37.765297, -122.442527, "2023-11-12T01:23:00Z")
+#find_route(37.770581, -122.442550, 37.765297, -122.442527)
+#print(get_uas())
+"""
