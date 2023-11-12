8d91468fcfc41fbc3c875e0e497dc43015233f82
ArranKooner <125759795+ArranKooner@users.noreply.github.com>
2023-11-11 19:48:01 -0800

    editied the api map

--- googleMaps.py
+++ googleMaps.py
@@ -15,16 +15,12 @@ data = res.read()
 print(data.decode("utf-8"))
 '''
 
-
-
 import http.client
-
+import certifi
 conn = http.client.HTTPSConnection("maps.googleapis.com")
 payload = ''
 headers = {}
-
 conn.request("GET", "/maps/api/place/nearbysearch/json?keyword=grocery&location=37.349200%252C-121.938562&radius=1500&fields=formatted_address,name,geometry&key=AIzaSyBlP3GItzc6nLlcHHQPMldzK3Jjsg-Gjdo", payload, headers)
 res = conn.getresponse()
 data = res.read()
-
 print(data.decode("utf-8"))
