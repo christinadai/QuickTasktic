9d991600c92e6b80d23708a2c8ffdb55d002bf4b
ArranKooner <125759795+ArranKooner@users.noreply.github.com>
2023-11-11 16:13:23 -0800

    api work
    arran finished the open ai api

--- openai_tester.py
+++ openai_tester.py
@@ -1,25 +1,16 @@
 # This code is for v1 of the openai package: pypi.org/project/openai
-from openai import OpenAI
-client = OpenAI()
+import openai
+import json
+client = openai.OpenAI(api_key="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")
+
+
 
 response = client.chat.completions.create(
     model="gpt-4",
     messages=[
     {
         "role": "user",
-        "content": "hi\n"
-    },
-    {
-        "role": "assistant",
-        "content": "Hello! How can I assist you today?\n"
-    },
-    {
-        "role": "user",
         "content": "give me JUST a number: AVERAGE time spent in grocery store without any extra words. don't say \"the average time spent in a grocery store is\" and don't say “minute”. the unit will be minutes, but only give the number back"
-    },
-    {
-        "role": "assistant",
-        "content": "41"
     }
     ],
     temperature=1,
@@ -28,3 +19,13 @@ response = client.chat.completions.create(
     frequency_penalty=0,
     presence_penalty=0
 )
+#print(response.json())
+#print(type(response.json()))
+
+jsonified_response=json.loads(response.json())
+#print(type(jsonified_response))
+
+print(jsonified_response["choices"][0]["message"]["content"])
+
+#print(json_ed["id"])
+#print(response.json()["id"])
